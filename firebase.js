acab8890512aa0a23cad4b01e92beedf7f23e4f6
mailtopilakhub-wq <mailtopilakhub@gmail.com>
2025-12-18 11:32:52 +0530

    Add Firebase configuration to firebase.js file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "<YOUR-API-KEY>...,
+ };
+
+const appEXPLAIN MOCK FLOW USING HTMLOWERS ParMODIFY}
